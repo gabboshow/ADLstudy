63811475f6983c67ded01a97ec8546ac1a014a34
gabboshow <gabbo.spina@gmail.com>
2014-06-18 09:52:33 +0200

    sencond patient

--- ADLstudy_code/main.m
+++ ADLstudy_code/main.m
@@ -2,6 +2,6 @@ clear
 close all
 
 data = xlsread('/media/Elements/Gabriele/ADL_study/All/Patient_2/SensorData/Kilani_170614.xlsx');
-
+a = datestr(data(:,1),'MM:SS');
 Partindex = 2;
 st_marker_ADL
