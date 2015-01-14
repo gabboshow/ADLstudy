eda8bfbf97417e466f339cff5e6a7b9d478279f2
gabboshow <gabbo.spina@gmail.com>
2015-01-14 19:04:10 +0100

    ciao

--- ADLstudy_code2/read_wristR.m
+++ ADLstudy_code2/read_wristR.m
@@ -1,39 +1,94 @@
 clear 
-close all
+% close all
 
-path_data = '\\code1\storage\2012-0362_icare4copd_ux\datasets\ADLstudy\ADL004\All_Data\B1\';
-data= fopen(strcat(path_data,'data_AnkleR.bin'));
+% path_data = '\\code1\storage\2012-0362_icare4copd_ux\datasets\ADLstudy\ADL004\All_Data\B1\';
+path_data = '/home/2012-0362_icare4copd/datasets/ADLstudy/ADL007/All_Data/B1/';
+% sensor = 'AnkleR';
+% sensor = 'WristR';
+% sensor = 'WristL';
+sensor = 'ECG1';
+% sensor = 'Sync';
+data= fopen(strcat(path_data,'data_',sensor,'.bin'));
 
 fs_shimmer = 170.67;
 fs_true = 1024/(ceil(1024/fs_shimmer));
 
 % read data
 more off
-ShimmerValues = fread(data,[6, Inf], 'uint16');
+if strcmp(sensor,'ECG1') || strcmp(sensor,'ECG2')
+    ShimmerValues = fread(data,[6, Inf], 'uint16');
+    data_acc = ShimmerValues(2:4,:)';
+    data_ecg = ShimmerValues(5:6,:)';
+    
+    time_plot = (0:size(data_acc,1)-1)/fs_true;
+
+    figure
+    subplot(2,1,1)
+    hold on
+    plot(time_plot,data_acc(:,1),'r');
+    plot(time_plot,data_acc(:,2),'g');
+    plot(time_plot,data_acc(:,3),'b');
+    xlabel('Time (seconds)');
+    ylabel('m/s^2');
+    legend('x','y','z');
+
+    subplot(2,1,2)
+    hold on
+    plot(time_plot,data_ecg(:,1),'r');
+    plot(time_plot,data_ecg(:,2),'g');
+    xlabel('Time (seconds)');
+    ylabel('');
+    legend('Channel 1','Channel 2');
+    
+elseif strcmp(sensor,'Sync') 
+    ShimmerValues = fread(data,[4, Inf], 'uint16');
+    
+    data_acc = ShimmerValues(2:4,:)';
+        
+    time_plot = (0:size(data_acc,1)-1)/fs_true;
+
+    figure
+    hold on
+    plot(time_plot,data_acc(:,1),'r');
+    plot(time_plot,data_acc(:,2),'g');
+    plot(time_plot,data_acc(:,3),'b');
+    xlabel('Time (seconds)');
+    ylabel('m/s^2');
+    legend('x','y','z');
 
 
-data_accx = ShimmerValues(1,:)';
-data_accy = ShimmerValues(2,:)';
-data_accz = ShimmerValues(3,:)';
+    
+else
+    ShimmerValues = fread(data,[7, Inf], 'uint16');
+    
+    data_acc = ShimmerValues(2:4,:)';
+    data_gyro = ShimmerValues(5:7,:)';
+    
+    time_plot = (0:size(data_acc,1)-1)/fs_true;
 
-data_acc = [data_accx,data_accy,data_accz];
-data_acc_calib = calibrate_shimmer('wristR',data_acc)./10000;
+    figure
+    subplot(2,1,1)
+    hold on
+    plot(time_plot,data_acc(:,1),'r');
+    plot(time_plot,data_acc(:,2),'g');
+    plot(time_plot,data_acc(:,3),'b');
+    xlabel('Time (seconds)');
+    ylabel('m/s^2');
+    legend('x','y','z');
+
+    subplot(2,1,2)
+    hold on
+    plot(time_plot,data_gyro(:,1),'r');
+    plot(time_plot,data_gyro(:,2),'g');
+    plot(time_plot,data_gyro(:,3),'b');
+    xlabel('Time (seconds)');
+    ylabel('alpha');
+    legend('x','y','z');
+    
+end
 
 
-figure
-time_plot = (0:size(data_acc,1)-1)/fs_true;
-plot(time_plot,data_acc,'DisplayName','sync_dataX');
 
-figure
-time_plot = (0:size(data_acc_calib,1)-1)/fs_true;
-hold on
-plot(time_plot,data_acc_calib(:,1),'r');
-plot(time_plot,data_acc_calib(:,2),'g');
-plot(time_plot,data_acc_calib(:,3),'b');
-xlabel('Time (seconds)');
-ylabel('m/s^2');
-legend('x','y','z');
-ylim([-15 15]);
 
 
 
