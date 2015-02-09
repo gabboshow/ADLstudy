bef73495947fae034b0350830e8449c45716835d
gabboshow <gabbo.spina@gmail.com>
2015-02-09 17:58:58 +0100

    ciao

--- ADLstudy_code2/initialization.m
+++ ADLstudy_code2/initialization.m
@@ -1,4 +1,4 @@
-machine = 2; % 0 Laptop, 1 Philips windows, 2 Philips clusters
+machine = 1; % 0 Laptop, 1 Philips windows, 2 Philips clusters
 
 switch machine
     case 0
@@ -9,8 +9,8 @@ switch machine
         path = '/home/2012-0362_icare4copd/datasets/ADLstudy/';
 end
 
-tests = {'B1','B2','E'};
-sensors = {'ecg','wristL','wristR','ankleR','sync'};
+tests = {'E'};%{'B1','B2','E'};
+sensors = {'ecg','wristL'};%,'wristR','ankleR','sync'};
 
 
 
